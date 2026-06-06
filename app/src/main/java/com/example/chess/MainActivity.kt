1e35c6b5fad3b5a7b225d25fd1d8072edbf38fd8
Asa Ryan Fletcher <167818887+AsaRy13@users.noreply.github.com>
2026-06-06 00:06:38 -0600

    Modified imports

--- app/src/main/java/com/example/chess/MainActivity.kt
+++ app/src/main/java/com/example/chess/MainActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.chess.ui.theme.ChessTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,6 +70,9 @@ fun Chess(name: String, modifier: Modifier = Modifier) {
     )
 
     Row(modifier = modifier) {
+        // I'm still new at Jetpack Compose, so at first this code block was just a bunch of
+        // repetitive code. Then I asked AI for help with a different issue and one of the things
+        // it recommended was to condense all of my repetitive code into these for loops.
         for (col in 0..7) {
             Column(modifier = Modifier.weight(1f)) {
                 for (row in 0..7) {
